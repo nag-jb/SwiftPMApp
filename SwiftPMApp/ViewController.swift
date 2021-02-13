081c2ad302c1293e7ea305129980b8e2fbd60061
Nagiko Tatsui <n.t.woody.0914@gmail.com>
2021-02-13 20:18:27 +0900

    Swift Package Managerの完成

--- SwiftPMApp/ViewController.swift
+++ SwiftPMApp/ViewController.swift
@@ -6,12 +6,22 @@
 //
 
 import UIKit
+//PKHUDのライブラリをimport(読み込み)
+import PKHUD
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    //画面が表示された直後に呼び出される
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        //PKHUDを表示
+        HUD.flash(.success, delay: 2.0)
     }
 
 
